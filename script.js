7665b549c9784297cb9cff2730d10e25016fbf39
edgyhyeji <153347353+edgyhyeji@users.noreply.github.com>
2024-01-06 14:25:49 -0600

    added js and timeblocks in html

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+const currentDate = dayjs();
+$('#currentDay').text(currentDate.format('MMM DD, YYYY'));
+const currentTime = dayjs();
+$('#currentTime').text(currentTime.format('h:mm a'));
+
+const currentHour = dayjs().format("HH");
+console.log(currentHour);
+console.log($(".row"));
+
+const timeBlock = $('.row');
+timeBlock.each(function(index){
+  console.log(($(this).attr("id").replace("hour-", "")));
+  
+  if (currentHour == $(this).attr("id").replace("hour-", "")) {
+    $(this).addClass("present");
+  }
+
+  else if (currentHour < $(this).attr("id").replace("hour-", "")) {
+    $(this).addClass("future");
+  }
+  
+  else {
+    $(this).addClass("past");
+  }
+})
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +47,11 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+// tackle top to bottom of page like a list. 
+
+// color code past present future, by utilizing current time. 
+
+// when i click the save button, it will save to localStorage
+// when page is loaded, saved text should render to correlating blocks
+
