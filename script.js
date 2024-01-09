de1f545b6385e634a2511885a913356bf8a0c8f4
edgyhyeji <153347353+edgyhyeji@users.noreply.github.com>
2024-01-08 19:29:01 -0600

    made changes in html and js. added new timeblock

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const currentDate = dayjs();
 $('#currentDay').text(currentDate.format('MMM DD, YYYY'));
 const currentTime = dayjs();
-$('#currentTime').text(currentTime.format('h:mm a'));
+$('#currentTime').text(currentTime.format('hh:mm a'));
 
 const currentHour = dayjs().format("HH");
 console.log(currentHour);
@@ -27,31 +27,22 @@ timeBlock.each(function(index){
   }
 })
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+$(".saveBtn").on("click", function() {
+  const timeBlockId = $(this).parent(".time-block").attr("id");
+  const userToDo = $(this).siblings(".description").val();
+
+  localStorage.setItem(timeBlockId, userToDo);
+
 });
 
-// tackle top to bottom of page like a list. 
+timeBlock.each(function() {
+  const timeBlockId = $(this).attr("id");
+  const storedToDo = localStorage.getItem(timeBlockId);
 
-// color code past present future, by utilizing current time. 
-
-// when i click the save button, it will save to localStorage
-// when page is loaded, saved text should render to correlating blocks
+  if (storedToDo) {
+    $(this).find(".description").val(storedToDo);
+  }
+  
+});
 
